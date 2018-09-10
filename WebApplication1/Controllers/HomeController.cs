3b3f65ed26312348a9e45d1b7d7c9c7776c3715a
Yuri Feng <livir_van@outlook.com>
2018-09-11 00:03:18 +0800

    添加项目文件。

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Product _myProduct = new Product()
+        {
+            ProductId = 1,
+            Name = "Micheal Jackson",
+            Description = "He is the best popular music singer in the world.",
+            Price = 12000,
+            Category = "American",
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_myProduct);
+        }
+    }
+}
