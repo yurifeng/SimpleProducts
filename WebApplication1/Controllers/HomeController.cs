0a1b5558254d422eeca20d1ba729e0204c7da160
yurifeng <yuri_feng@outlook.com>
2018-09-11 11:27:34 +0800

    创建新视图

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 添加Product
+        /// </summary>
         private readonly Product _myProduct = new Product()
         {
             ProductId = 1,
@@ -14,14 +17,37 @@ namespace WebApplication1.Controllers
             Category = "American",
         };
 
-        // GET: Home
+        /// <summary>
+        /// 添加动作
+        /// localhost:12306/Home/index
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View(_myProduct);
         }
 
+        /// <summary>
+        /// 添加动作
+        /// localhost:12306/Home/NameAndPrice
+        /// </summary>
+        /// <returns></returns>
         public ActionResult NameAndPrice()
         {
+            return View(_myProduct);
+        }
+
+        /// <summary>
+        /// 添加动作
+        /// localhost:12306/Home/DemoExpressions
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DemoExpressions()
+        {
+            ViewBag.ProductCount = 0;
+            ViewBag.ExpressShip = false;
+            ViewBag.ApplyDiscount = true;
+            ViewBag.Supplier = null;
             return View(_myProduct);
         }
     }
